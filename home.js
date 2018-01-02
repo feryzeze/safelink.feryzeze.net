6f739af770bc774008988839aadaeb5ae3e9e828
feryzeze <33867527+feryzeze@users.noreply.github.com>
2018-01-03 00:47:28 +0700

    Update home.js

--- home.js
+++ home.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="what-is-forex-understanding-forex-forex.html"}
 if(x=="2"){xxx="trandingbitcoin.html"}
 if(x=="3"){xxx="why-do-we-need-buy-insurance.html"}
 if(x=="4"){xxx="how-to-design-narrow-living-room.html"}
-if(x=="5"){xxx="10-recommended-forex-brokers-for.html"}a_to_ve[i].href="https://www.dancoeg.ga/2015/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="10-recommended-forex-brokers-for.html"}a_to_ve[i].href="https://www.dancoeg.ga/2018/01/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
