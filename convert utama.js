2dd51254eec392034af8c2d959d5b6624cb8a596
feryzeze <33867527+feryzeze@users.noreply.github.com>
2018-02-21 21:39:03 +0700

    Update convert utama.js

--- convert utama.js
+++ convert utama.js
@@ -21,12 +21,12 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*4)+ 1);
 var xxx=null;
-if(x=="1"){xxx="advantages-of-being-home-business-owner.html"}
-if(x=="2"){xxx="whats-is-forex.html"}
-if(x=="3"){xxx="practice-environmental-qwareness-to-our.html"}
-if(x=="4"){xxx="why-do-we-need-buy-insurance.html"}
-if(x=="5"){xxx="tips-on-choose-furniture-for-minimalist.html"}
-if(x=="6"){xxx="how-to-design-narrow-living-room.html"}a_to_ve[i].href="http://safelink.feryzeze.net/2017/11/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="tips-on-choose-furniture-for-minimalist.html"}
+if(x=="2"){xxx="advantages-of-being-home-business-owner.html"}
+if(x=="3"){xxx="whats-is-forex.html"}
+if(x=="4"){xxx="practice-environmental-qwareness-to-our.html"}
+if(x=="5"){xxx="why-do-we-need-buy-insurance.html"}
+if(x=="6"){xxx="how-to-design-narrow-living-room.html"}a_to_ve[i].href="http://safelink.feryzeze.net/2015/12/"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
